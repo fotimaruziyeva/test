f7a880ee83d57d21d7a1c91233349192eb0aa08f
fotimaruziyeva <ruziyevafotima12@gmail.com>
2024-07-20 11:55:59 +0500

    test

---
+++ js/question.js
@@ -0,0 +1,57 @@
+let questions=[
+    /***1 */
+    {
+         numb:1,
+        question:"HTML ni kengaytmasi",
+        answer:"A. hyper text markup language",
+        options:[
+            "A. hyper text markup language",
+            "B. Cascading style sheet",
+            "C. JS"
+        ]
+    },
+    /**2 */
+    {
+        numb:2,
+        question:"strong, b teglarining qanday farqi bor?",
+        answer:"A. strong matnning qolgan qismiga qaraganda mos ravishda katta ahamiyatga ega yoki ko'proq ahamiyatga ega va urg'uli ekanligini bildiradi. Bu semantik ma'noga ega",
+        options:[
+            "A. strong matnning qolgan qismiga qaraganda mos ravishda katta ahamiyatga ega yoki ko'proq ahamiyatga ega va urg'uli ekanligini bildiradi. Bu semantik ma'noga ega",
+            "B. hech qanday farqi yuq",
+            "C. b matnning qolgan qismiga qaraganda mos ravishda katta ahamiyatga ega yoki ko'proq ahamiyatga ega va urg'uli ekanligini bildiradi. Bu semantik ma'noga ega "
+        ]
+    },
+    // 3//
+    {
+        numb:3,
+        question:"Jadvaldagi ustunlarni birlashtirish uchun qaysi atributdan foydalanamiz? ",
+        answer:"C. colspan",
+        options:[
+            "A. strong",
+           "B.  rowspan" ,
+           "C. colspan"
+        ]
+    },
+    // 4-//
+    {
+        numb:4,
+        question:" HTMLda havola yani link qaysi teg bilan yaratiladi?",
+        answer: "B. a href=''",
+        options:[
+            "A. form",
+            "B. a href=''",
+            "C url"
+        ]
+    },
+    // 5
+    {
+        numb:5,
+        question:" Qaysi teg yordamida tartibli ro'yhat yaratishimiz mumkin?",
+        answer:"A. ol",
+        options:[
+            "A. ol",
+            "B  ul",
+            "C li"
+        ]
+    }
+]
